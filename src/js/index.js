81b1ce066ccf3f04765bada4f4534061da34b183
JL10 <126992358+JLizcano10@users.noreply.github.com>
2023-10-10 17:20:50 -0500

    button loadMore and initial eventListener

--- src/js/index.js
+++ src/js/index.js
@@ -7,16 +7,17 @@ import { createMarkup } from './createMarkup';
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
 };
-
-refs.searchForm.addEventListener('submit', searchHandle);
+let page;
 
 function searchHandle(e) {
   e.preventDefault();
+  page = 1;
   refs.gallery.innerHTML = '';
   const request = e.target.elements.searchQuery.value;
   if (request !== '') {
-    getPhotos(request).then(data => {
+    getPhotos(request, page).then(data => {
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
       const dataArray = data.hits;
       if (dataArray.length !== 0) {
@@ -33,3 +34,8 @@ function searchHandle(e) {
     );
   }
 }
+function loadMoreHandle() {
+  console.log('alo?');
+}
+refs.searchForm.addEventListener('submit', searchHandle);
+refs.loadMore.addEventListener('submit', loadMoreHandle);
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 const url = 'https://pixabay.com/api/?key=39922644-bcfb57c4c3a31264e272bb894';
-async function getPhotos(request) {
+async function getPhotos(request, page) {
   try {
     const response = await axios.get(url, {
       params: {
@@ -9,7 +9,7 @@ async function getPhotos(request) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        page: 1,
+        page: page,
         per_page: 40,
       },
     });
